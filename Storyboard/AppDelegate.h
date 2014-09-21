b8f8448bf705d0d0275c0f20303c0c319233bc39
New <eric.braen@gmail.com>
2014-09-20 23:12:30 -0700

    first commit

---
+++ Storyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Storyboard
+//
+//  Created by New on 9/20/14.
+//  Copyright (c) 2014 New. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
